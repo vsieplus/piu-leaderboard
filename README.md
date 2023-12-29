9e46ae1208ea22e014da63c9bfd720502858a53e
vsieplus <35880073+vsieplus@users.noreply.github.com>
2023-12-29 01:11:25 -0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# piu-leaderboard
+
+*piu-leaderboard* is a discord bot which automatically tracks leaderboard updates for Pump it Up Phoenix.
+
+## Commands
+
---
+++ bot.py
@@ -0,0 +1,48 @@
+import os
+import random
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        await ctx.send('Command not found.')
+    elif isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+@bot.command(name='99', help='Responds with random quote')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+bot.run(TOKEN)
