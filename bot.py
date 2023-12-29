27383c89260deeb1f4a2d930eb9d2d579cc1a9fe
vsieplus <35880073+vsieplus@users.noreply.github.com>
2023-12-29 15:20:52 -0800

    Update bot commands

--- bot.py
+++ bot.py
@@ -1,27 +1,36 @@
+# bot.py
+
 import os
 import random
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
+
+from leaderboard import Leaderboard
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+bot_channel_id = None
+
+leaderboards = dict()
 
 @bot.event
 async def on_ready():
-    guild = discord.utils.get(bot.guilds, name=GUILD)
+    for guild in bot.guilds:
+        leaderboards[guild.name] = Leaderboard()
 
-    print(
-        f'{bot.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+        print(
+            f'{bot.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+    update_leaderboard.start()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -30,19 +39,37 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
 
-@bot.command(name='99', help='Responds with random quote')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
+@bot.command(name='track', help='Begin tracking a player\'s scores')
+async def track(ctx, player_id: str):
+    if leaderboards[ctx.guild.name].add_player(player_id):
+        await ctx.send(f'Now tracking player {player_id}.')
+    else:
+        await ctx.send(f'Player {player_id} is already being tracked.')
 
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
+@bot.command(name='untrack', help='Stop tracking a player\'s scores')
+async def untrack(ctx, player_id: str):
+    if leaderboards[ctx.guild.name].remove_player(player_id):
+        await ctx.send(f'No longer tracking player {player_id}.')
+    else:
+        await ctx.send(f'Player {player_id} is not being tracked.')
+
+@bot.command(name='query', help='Query a player\'s rank on a level')
+async def query(ctx, player_id: str, level_id: str):
+    score = leaderboards[ctx.guild.name].query_score(player_id, level_id)
+    if score is None:
+        await ctx.send(f'{player_id} is not on the leaderboard for {level_id}.')
+    else:
+        await ctx.send(embed=score.embed())
+
+@tasks.loop(minutes=5)
+async def update_leaderboard():
+    Leaderboard.update()
+
+    for guild in bot.guilds:
+        # only send updates in the 'piu-scores' channel
+        for channel in guild.text_channels:
+            if channel.name == 'piu-scores':
+                await leaderboards[guild.name].update_leaderboard(channel)
 
 
 bot.run(TOKEN)
