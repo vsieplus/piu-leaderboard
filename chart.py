8d24258fcaec689d731f3572ee6a7b17b1a200f4
vsieplus <35880073+vsieplus@users.noreply.github.com>
2023-12-30 13:53:29 -0800

    Chart | inherit from dict

--- chart.py
+++ chart.py
@@ -8,21 +8,17 @@ MODE_ABBREV = {
     'Co-op' : 'Co-op '
 }
 
-class Chart:
+def get_chart_id(title, mode, level) -> str:
+    mode_txt = 'Unknown'
+    if mode in MODE_ABBREV:
+        mode_txt = MODE_ABBREV[mode]
+
+    return f'{title} {mode_txt}{level}'
+
+class Chart(dict):
     def __init__(self, title, mode, level, leaderboard_id, thumbnail_url):
-        self.title = title
-        self.mode = mode
-        self.level = level
-        self.chart_id = self.get_chart_id()
-        self.leaderboard_id = leaderboard_id
-        self.thumbnail_url = thumbnail_url
+        dict.__init__(self, title=title, mode=mode, level=level, chart_id=get_chart_id(title, mode, level),
+                      leaderboard_id=leaderboard_id, thumbnail_url=thumbnail_url)
 
     def get_leaderboard_url(self) -> str:
-        return f'{BASE_URL}?no={self.leaderboard_id}'
-
-    def get_chart_id(self) -> str:
-        mode_txt = 'Unknown'
-        if self.mode in MODE_ABBREV:
-            mode_txt = MODE_ABBREV[self.mode]
-
-        return f'{self.title} {mode_txt}{self.level}'
+        return f'{BASE_URL}?no={self['leaderboard_id']}'
