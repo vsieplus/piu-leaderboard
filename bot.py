4b1e278ed45259aecc77ae1ce6c62249b59e2185
vsieplus <35880073+vsieplus@users.noreply.github.com>
2024-01-15 12:44:31 -0800

    Bot | Customize help message

--- bot.py
+++ bot.py
@@ -9,19 +9,52 @@ from dotenv import load_dotenv
 
 from leaderboard import GuildLeaderboard, Leaderboard
 
+class CustomHelpCommand(commands.HelpCommand):
+    async def send_bot_help(self, mapping):
+        # Create a list to hold the pages
+        pages = []
+
+        # Add a page for each cog and its commands
+        for category, commands in mapping.items():
+            if category is None:
+                category = 'No Category'
+            pages.append(f'{category}:')
+
+            max_cmd_width = max(len(command.qualified_name) for command in commands)
+            max_sig_width = max(len(self.get_command_signature(command)) for command in commands)
+
+            # sort commands alphabetically
+            commands = sorted(commands, key=lambda command: command.qualified_name)
+
+            for command in commands:
+                cmd_spaces_to_add = max_cmd_width - len(command.qualified_name) + 2
+                cmd_spaces = ' ' * cmd_spaces_to_add
+
+                sig_spaces_to_add = max_sig_width - len(self.get_command_signature(command)) + 2
+                sig_spaces = ' ' * sig_spaces_to_add
+
+                pages.append(f'\t{command.qualified_name}{cmd_spaces}{self.get_command_signature(command)}{sig_spaces}{command.help}')
+
+        pages.append('\nParameters (case-insensitive): ')
+        pages.append('\tplayer_id  name[#tag]                       | if #tag is not specified, all scores with a matching name will be returned\n'
+                     '\tlevel_id   "Song title (S/D/Co-op)(Level)"  | must be enclosed in quotes; for Co-op chart levels, use x2, x3, etc...')
+
+
+
+        await self.get_destination().send('```' + '\n'.join(pages) + '```')
+
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_NAME = 'piu-leaderboard'
+
+leaderboards = dict()
+leaderboard = Leaderboard()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-bot_channel_id = None
-
-leaderboards = dict()
-leaderboard = Leaderboard()
-
-CHANNEL_NAME = 'piu-leaderboard'
 
 @bot.event
 async def on_ready():
@@ -42,15 +75,16 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
 
+
 @bot.command(name='track', help='Begin tracking a player\'s scores')
-async def track(ctx, player_id: str):
+async def track(self, ctx, player_id: str):
     if await leaderboards[ctx.guild.name].add_player(player_id):
         await ctx.send(f'Now tracking player {player_id}.')
     else:
         await ctx.send(f'Player {player_id} is already being tracked.')
 
 @bot.command(name='untrack', help='Stop tracking a player\'s scores')
-async def untrack(ctx, player_id: str):
+async def untrack(self, ctx, player_id: str):
     if await leaderboards[ctx.guild.name].remove_player(player_id):
         await ctx.send(f'No longer tracking player {player_id}.')
     else:
@@ -77,4 +111,5 @@ async def update_leaderboard():
             if channel.name == CHANNEL_NAME:
                 await leaderboards[guild.name].get_leaderboard_updates(channel)
 
+bot.help_command = CustomHelpCommand()
 bot.run(TOKEN)
