46f033f13f7c89635098a592d005707863926cd2
vsieplus <35880073+vsieplus@users.noreply.github.com>
2024-06-29 16:00:52 -0700

    Move 'get_rank_suffix()' to util.py

---
+++ src/util.py
@@ -0,0 +1,20 @@
+# util.py
+
+RANKING_SUFFIXES = {
+    1 : 'st',
+    2 : 'nd',
+    3 : 'rd',
+    4 : 'th',
+    5 : 'th',
+    6 : 'th',
+    7 : 'th',
+    8 : 'th',
+    9 : 'th',
+    0 : 'th',
+}
+
+def get_rank_suffix(cls, rank) -> str:
+    if rank >= 11 and rank <= 13:
+        return 'th'
+
+    return RANKING_SUFFIXES[rank % 10]
